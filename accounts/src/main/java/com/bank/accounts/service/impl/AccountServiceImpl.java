40fcabde27dda3671eff42dfe26217472471d924
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2024-07-16 00:06:36 +0530

    feat: added create account service

--- accounts/src/main/java/com/bank/accounts/service/impl/AccountServiceImpl.java
+++ accounts/src/main/java/com/bank/accounts/service/impl/AccountServiceImpl.java
@@ -1,11 +1,17 @@
 package com.bank.accounts.service.impl;
 
+import com.bank.accounts.constants.AccountsConstants;
 import com.bank.accounts.dto.CustomerDto;
+import com.bank.accounts.entity.AccountsEntity;
+import com.bank.accounts.entity.CustomerEntity;
+import com.bank.accounts.mapper.CustomerMapper;
 import com.bank.accounts.repository.AccountsRepository;
 import com.bank.accounts.repository.CustomerRepository;
 import com.bank.accounts.service.IAccountService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -17,6 +23,23 @@ public class AccountServiceImpl implements IAccountService {
      */
     @Override
     public void createAccount(CustomerDto customerDto) {
-
+        CustomerEntity customer = CustomerMapper.mapToCustomer(customerDto, new CustomerEntity());
+        CustomerEntity savedCustomer = customerRepository.save(customer);
+        accountsRepository.save(createNewAccount(savedCustomer));
     }
+    /**
+     * @param customer - Customer Object
+     * @return the new account details
+     */
+    private AccountsEntity createNewAccount(CustomerEntity customer) {
+        AccountsEntity newAccount = new AccountsEntity();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccount_type(AccountsConstants.SAVINGS);
+        newAccount.setBranch_address(AccountsConstants.ADDRESS);
+        return newAccount;
+    }
+
 }
