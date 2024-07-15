1617de3a0b615e2188aace8d25c0e5d122d1afb4
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2024-07-15 23:51:57 +0530

    feat: added account create api

--- accounts/src/main/java/com/bank/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/bank/accounts/controller/AccountsController.java
@@ -1,12 +1,19 @@
 package com.bank.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bank.accounts.constants.AccountsConstants;
+import com.bank.accounts.dto.CustomerDto;
+import com.bank.accounts.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path = "/api",produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AccountsController {
-    @GetMapping("home")
-    public String sayHello() {
-        return "Hi World";
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 }
