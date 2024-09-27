dfc841bc63dbce273f9ff80248c7bcc388c81956
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2024-09-27 23:31:04 +0530

    feat: added retry pattern

--- gatewayserver/src/main/java/com/bank/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/bank/gatewayserver/GatewayserverApplication.java
@@ -6,7 +6,9 @@ import org.springframework.cglib.core.Local;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
@@ -39,7 +41,13 @@ public class GatewayserverApplication {
 				.route(
 						p-> p.path("/bank/loans/**")
 								.filters(f->f.rewritePath("/bank/loans/(?<segment>.*)","/${segment}")
-										.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+										.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+										.retry(retryConfig -> retryConfig.setRetries(3)
+												.setMethods(HttpMethod.GET)
+												.setBackoff(
+														Duration.ofMillis(100),Duration.ofMillis(1000),2,true
+												)
+										))
 								.uri("lb:LOANS")
 				).build();
 	}
